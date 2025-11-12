6453d1a3aa49c8813f2461eb3e54d9044faf8b72
Wiktoria Lisson <wilisson@3-D-12.42heilbronn.de>
2025-11-12 15:57:13 +0100

    header, utils && start of the main file

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,20 @@
 /*   By: wilisson <wilisson@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 16:46:39 by wilisson          #+#    #+#             */
-/*   Updated: 2025/11/11 17:11:48 by wilisson         ###   ########.fr       */
+/*   Updated: 2025/11/12 15:55:55 by wilisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char buffer[BUFFER_SIZE + 1];
+	static char	*saved;
+	char		*line;
 
-    if(fd < 0 || buffer <= 0)
-        return(-1);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	saved = read_file(fd, saved);
+	if (!saved)
+		return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,21 @@
 /*   By: wilisson <wilisson@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 16:46:41 by wilisson          #+#    #+#             */
-/*   Updated: 2025/11/11 16:58:53 by wilisson         ###   ########.fr       */
+/*   Updated: 2025/11/12 15:56:14 by wilisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
 
 
 #endif
